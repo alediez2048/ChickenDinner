9a61780099919f8ca1df7cea9eeba2f5caceb888
Jorge Diez <alediez240a@Jorges-MacBook-Pro.local>
2017-07-28 15:56:50 -0500

    Routes for data queries

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,51 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/user');
 
-// GET / register
+// GET /register
 router.get('/register', function(req, res, next) {
-	return res.render('register', { title: 'Sign Up' });
+  return res.render('register', { title: 'Sign Up' });
 });
 
-// POST / register
+// POST /register
 router.post('/register', function(req, res, next) {
-	return res.send('User Created');
-});
+  if (req.body.email &&
+    req.body.name &&
+    req.body.favoriteBook &&
+    req.body.password &&
+    req.body.confirmPassword) {
+
+      // confirm that user typed same password twice
+      if (req.body.password !== req.body.confirmPassword) {
+        var err = new Error('Passwords do not match.');
+        err.status = 400;
+        return next(err);
+      }
+
+      // create object with form input
+      var userData = {
+        email: req.body.email,
+        name: req.body.name,
+        favoriteBook: req.body.favoriteBook,
+        password: req.body.password
+      };
+
+      // use schema's `create` method to insert document into Mongo
+      User.create(userData, function (error, user) {
+        if (error) {
+          return next(error);
+        } else {
+          return res.redirect('/profile');
+        }
+      });
+      
+    } else {
+      var err = new Error('All fields required.');
+      err.status = 400;
+      return next(err);
+    }
+})
+
 // GET /
 router.get('/', function(req, res, next) {
   return res.render('index', { title: 'Home' });
@@ -24,5 +60,7 @@ router.get('/about', function(req, res, next) {
 router.get('/contact', function(req, res, next) {
   return res.render('contact', { title: 'Contact' });
 });
+
+module.exports = router;
 
 module.exports = router;
